05e416bd2ae5a74183db1d7d99823a3aae055c95
gahyeon <1000ji1@naver.com>
2025-10-24 13:43:41 +0900

    feat: 보안구성

---
+++ MSA/com.spring-cloud.eureka.client.auth/src/main/java/com/spring_cloud/eureka/client/auth/AuthService.java
@@ -0,0 +1,45 @@
+package com.spring_cloud.eureka.client.auth;
+
+import java.sql.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class AuthService {
+
+	@Value("${spring.application.name}")
+	private String issuer;
+
+	@Value("${service.jwt.access-expiration}")
+	private Long accessExpiration;
+
+	private final SecretKey secretKey;
+
+	public AuthService(@Value("${service.jwt.secret-key}") String secretKey) {
+		this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+	}
+
+	public String createAccessToken(String user_id) {
+		return Jwts.builder()
+			// 사용자 ID를 클레임으로 설정
+			.claim("user_id", user_id)
+			.claim("role", "ADMIN")
+			// JWT 발행자를 설정
+			.issuer(issuer)
+			// JWT 발행 시간을 현재 시간으로 설정
+			.issuedAt(new Date(System.currentTimeMillis()))
+			// JWT 만료 시간을 설정
+			.expiration(new Date(System.currentTimeMillis() + accessExpiration))
+			// SecretKey를 사용하여 HMAC-SHA512 알고리즘으로 서명
+			.signWith(secretKey, io.jsonwebtoken.SignatureAlgorithm.HS512)
+			// JWT 문자열로 컴팩트하게 변환
+			.compact();
+	}
+}
