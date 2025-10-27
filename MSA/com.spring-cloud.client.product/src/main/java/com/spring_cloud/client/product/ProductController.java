02c567b76b0c148040ca76e477575310a5973ffa
gahyeon <1000ji1@naver.com>
2025-10-27 15:52:06 +0900

    feat: config 설정

--- MSA/com.spring-cloud.client.product/src/main/java/com/spring_cloud/client/product/ProductController.java
+++ MSA/com.spring-cloud.client.product/src/main/java/com/spring_cloud/client/product/ProductController.java
@@ -1,18 +1,24 @@
 package com.spring_cloud.client.product;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
+@RequestMapping("/product")
 public class ProductController {
 
     @Value("${server.port}") // 애플리케이션이 실행 중인 포트를 주입받습니다.
     private String serverPort;
 
-    @GetMapping("/product")
-    public String getProduct(){
-        return "Product info!!!! From port : " + serverPort;
+    @Value("${message}")
+    private String message;
+
+    @GetMapping
+    public String getProduct() {
+        return "Product detail from PORT : " + serverPort + " and message : " + this.message ;
     }
 }
